f8564fed7e8282584ae72944cb72cebdb27a7b5d
Jackson <jaksondikison@gmail.com>
2025-08-16 13:03:33 -0700

    Add attributes and event handlers

---
+++ my-input.js
@@ -0,0 +1,13 @@
+function MyInput({ formState, index }) {
+  return hlx`
+    <input 
+      value=${formState.value[index]}
+      oninput=${(event) => {
+        formState.value = {
+          ...formState.value,
+          [index]: event.target.value,
+        };
+      }}
+    />
+  `;
+}
